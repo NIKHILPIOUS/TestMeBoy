3d7202a88fc8ea92dd86f06c1e41a3a726d5c3c9
NIKHILPIOUS <nikiip3@gmail.com>
2024-03-11 04:41:49 -0400

    commit from acer 3

--- SampleModule/src/main/java/org/example/Main.java
+++ SampleModule/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Mac Threegit ");
+        System.out.println("Hello Mac Three ");
     }
 
     public void pleaseTouchMe(){
